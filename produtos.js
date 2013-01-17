7f2c68056a36a943e4e70e0c1ec76c183744aad0
nutrituragro <nutriagro.negocio@gmail.com>
2013-01-17 20:06:19 -0200

    Update produtos.js
    - '01 ': {' Pacote turismo data 29-31/03', '400,00'},

--- produtos.js
+++ produtos.js
@@ -1,7 +1,4 @@
 email_cobranca = 'suporte@lojamodelo.com.br';
 
 produtos={
-  '11': ['Bermuda amarela de nylon', '15,00'],
-  '12': ['Calça jeans', '98,50'],
-  '13': ['Carteira de couro', '50,22']
-};
+  01 -   { '
